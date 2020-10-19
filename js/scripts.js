f4e9d3ba8e7bed7751dba8fc171b7a360285f8c3
Njogu-brian <briannjogu85@gmail.com>
2020-10-20 00:39:17 +0300

    add order prototype

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@ function Order (customize, cheese) {
   this.flavor = 200;
   this.pizzaPrice = 0;
   this.drinks = 100;
+}
+Order.prototype.pizzaCost = function () {
+  if (this.customSize === "Small 10.") {
+    this.pizzaPrice += 500;
+  } else if (this.customSize === "Medium 14.") {
+    this.pizzaPrice += 700;
+  } else if (this.customSize === "Large.") {
+    this.pizzaPrice += 950;
+  } else if (this.customSize) === "Jumbo.") {
+    this.pizzaPrice +=1400
+  }
+  if (this.cheese === "cheese") {
+    this.pizzaPrice += 200;
+  } else if (this.cheese === "light cheese") {
+    this.pizzaPrice += 100;
+  } else if (this.cheese === "extra cheese") {
+    this.pizzaPrice += 250;
+  }
+  this.pizzaPrice += this.crust;
+  this.pizzaPrice += this.toppings;
+  this.pizzaPrice += this.flavor;
+  this.pizzaPrice += this.drinks
+  return this.pizzaPrice;
