f3dc9d1db47d6c80e60bb6fa1fea2ef95440169c
Njogu-brian <briannjogu85@gmail.com>
2020-10-20 00:34:45 +0300

    add function order

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var totalPriceArray = [];
+function Order (customize, cheese) {
+  this.customize=customize
+  this.crust = 100;
+  this.toppings = 150;
+  this.flavor = 200;
+  this.pizzaPrice = 0;
+  this.drinks = 100;
