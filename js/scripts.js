ad1a966817005504a05d175d6e095c96809927c0
Njogu-brian <briannjogu85@gmail.com>
2020-10-20 07:08:45 +0300

    remove drinks option

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,40 @@ Order.prototype.pizzaCost = function () {
   } else if (this.customSize) === "Jumbo.") {
     this.pizzaPrice +=1400
   }
-  if (this.cheese === "cheese") {
-    this.pizzaPrice += 200;
-  } else if (this.cheese === "light cheese") {
-    this.pizzaPrice += 100;
-  } else if (this.cheese === "extra cheese") {
-    this.pizzaPrice += 250;
-  }
   this.pizzaPrice += this.crust;
   this.pizzaPrice += this.toppings;
   this.pizzaPrice += this.flavor;
   this.pizzaPrice += this.drinks
   return this.pizzaPrice;
+}
+Order.prototype.finalCost = function () {
+  var cartTotalPrice = 0;
+  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+    cartTotalPrice += totalPriceArray[arrayElement];
+  }
+  return cartTotalPrice;
+}
+function Address (streetAddress, city, county) {
+  this.streetAddress = streetAddress;
+  this.city = city;
+  this.county = county;
+  this.deliveryAddress = (streetAddress + "  " + city + ", " + county);
+}
+//User Interface
+$(document).ready(function(event) {
+
+  $("form#custom-pizza").submit(function(event) {
+    event.preventDefault();
+    var customSize = $("select#size").val();
+    var crust = $("select#crust").val();
+    var toppings = $("select#toppings").val();
+    var flavor = $("select#flavor").val();
+    var pizzaDetails = (customSize + " - " + crust + ", " + cheese + ", " + toppings + ", "  + flavor);
+    var newPizzaOrder = new Order(customSize, cheese);
+    newPizzaOrder.pizzaCost();
+    totalPriceArray.push(newPizzaOrder.pizzaPrice);
+    $("#pizza-details-dropdown").show();
+    $("#final-cost").text(newPizzaOrder.finalCost());
+    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+    $("#size, #crust, #cheese, #toppings, #flavor").val("");
+  });
