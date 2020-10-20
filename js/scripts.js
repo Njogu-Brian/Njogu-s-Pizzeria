177690604c6e26c63be9ed340dfe847ab7393870
Njogu-brian <briannjogu85@gmail.com>
2020-10-20 11:58:49 +0300

    fix bugs and errors in formular

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
-//business logic
-var totalPriceArray = [];
-function Order (customize) {
+
+// Business Logic
+var totalPriceArray = []; //Only global variable in code
+function Order (customSize, cheese) {
   this.customSize = customSize;
   this.crust = 0;
+  this.cheese = cheese;
   this.toppings = 150;
+  this.flavor = 200;
   this.pizzaPrice = 0;
   this.sidePrice = 100;
 }
 Order.prototype.pizzaCost = function () {
-  if (this.customSize === "small") {
-    this.pizzaPrice += 500;
-  } else if (this.customSize === "medium") {
-    this.pizzaPrice += 700;
-  } else if (this.customSize === "large") {
-    this.pizzaPrice += 950;
-  } 
-  else if (this.customSize === "jumbo") {
-    this.pizzaPrice += 1400;
+  if (this.customSize === "Small 10 in.") {
+    this.pizzaPrice += 300;
+  } else if (this.customSize === "Medium 14 in.") {
+    this.pizzaPrice += 550;
+  } else if (this.customSize === "Large 18 in.") {
+    this.pizzaPrice += 750;
+  } else if (this.customSize === "Jumbo 22 in.") {
+    this.pizzaPrice += 1200;
+  }
+  if (this.cheese === "cheese") {
+    this.pizzaPrice += 100;
+  } else if (this.cheese === "light cheese") {
+    this.pizzaPrice += 50;
+  } else if (this.cheese === "extra cheese") {
+    this.pizzaPrice += 150;
   }
   this.pizzaPrice += this.crust;
   this.pizzaPrice += this.toppings;
+  this.pizzaPrice += this.flavor;
   return this.pizzaPrice;
 }
 Order.prototype.finalCost = function () {
@@ -35,21 +45,24 @@ function Address (streetAddress, city, county) {
   this.county = county;
   this.deliveryAddress = (streetAddress + "  " + city + ", " + county);
 }
-//User Interface
+//User Interface Logic
 $(document).ready(function(event) {
 
   $("form#custom-pizza").submit(function(event) {
     event.preventDefault();
     var customSize = $("select#size").val();
     var crust = $("select#crust").val();
+    var cheese = $("select#cheese").val();
     var toppings = $("select#toppings").val();
-    var pizzaDetails = (customSize + " - " + crust + ", " + toppings + ", ");
-    var newPizzaOrder = new Order(customSize);
+    var flavor = $("select#flavor").val();
+    var pizzaDetails = (customSize + " - " + crust + ", " + cheese + ", " + toppings + ", "  + flavor);
+    var newPizzaOrder = new Order(customSize, cheese);
+    newPizzaOrder.pizzaCost();
     totalPriceArray.push(newPizzaOrder.pizzaPrice);
     $("#pizza-details-dropdown").show();
     $("#final-cost").text(newPizzaOrder.finalCost());
     $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
-    $("#size, #crust, #toppings, #number").val("");
+    $("#size, #crust, #cheese, #toppings, #flavor").val("");
   });
   $("#pizza-details-dropdown").click(function() {
     $("#pizza-details").toggle();
